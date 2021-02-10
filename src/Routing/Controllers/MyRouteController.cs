de2ad850f6828539191228fa771ac1ed48854bfd
Amir Rad <afrasia@algonquincollege.com>
2021-02-09 23:48:17 -0500

    First commit

---
+++ src/Routing/Controllers/MyRouteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Docs.Samples;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Routing.Controllers
+{
+    public class MyRouteController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return ControllerContext.MyDisplayRouteInfo(); ;
+        }
+    }
+}
